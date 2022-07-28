146896db832c3b50c8fbe421ce8ae7b5fdab70a4
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-07-28 17:58:03 +0300

    Homework for Seminar 3

---
+++ Homework_task_23_Cubes/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число: ");
+int userNum = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Кубы чисел от 1 до {userNum} -> ");
+for(int i =1; i <= userNum; i++)
+    Console.Write($"{i*i*i}, ");
